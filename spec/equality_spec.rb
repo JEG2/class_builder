c48d4a0b2990bf348bbfe364444a097195f5096f
James Edward Gray II <james@graysoftinc.com>
2014-11-21 14:31:25 -0600

    Starting to think about equality.

---
+++ spec/equality_spec.rb
@@ -0,0 +1,11 @@
+require_relative "spec_helper"
+
+require_relative "../lib/class_builder"
+
+describe ClassBuilder do
+  context "equality" do
+    it "tests equality using all readers by default" do
+      
+    end
+  end
+end
